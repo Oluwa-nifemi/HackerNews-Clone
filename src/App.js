ae97f9e8c5472f22222c7e08cc946481a5d35873
Nifemi <deyeminifemi@gmail.com>
2019-07-26 22:37:52 +0100

    Created comments component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav/Nav'
 import AuthorDetails from './components/author-details/AuthorDetails'
 import ArticleDetails from './components/article-details/ArticleDetails'
+import Comment from './components/comment/Comment'
 
 function App() {
   return (
     <div className="App">
       {/* <Nav/> */}
       {/* <AuthorDetails author="sohkyamung" date="9/9/2014, 4:37 AM" karma={23485}/> */}
-      <ArticleDetails title="The art of dancing" author="sohkyamung" date="9/9/2014, 4:37 AM" comments={4}/>
+      {/* <ArticleDetails title="The art of dancing" author="sohkyamung" date="9/9/2014, 4:37 AM" comments={4}/> */}
+      <Comment 
+        author="sohkyamung" 
+        comment="This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating. Thank you.This is really good and motivating." 
+        date="9/9/2014, 4:37 AM" 
+      />
     </div>
   );
 }
---
+++ src/components/comment/Comment.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './comment.css'
+
+export default function Comment({ author, comment, date  }){
+    return (
+        <div className="comment">
+            <p className="comment__details">
+                by <a href="#" className="underline">{ author }</a> on { date }
+            </p>
+            <p className="comment__text">
+                { comment }
+            </p>
+        </div>
+    )
+}
+
+Comment.propTypes = {
+    author: PropTypes.string.isRequired,
+    comment: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+};
